d2a854abddce2843a7bc4df3c58ab54d3a8893a0
Purin <meebotsompurin@gmail.com>
2026-08-14 22:59:57 +0700

    feat: fetch and display categories on frontend and resolve CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 
